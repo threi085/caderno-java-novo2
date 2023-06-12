8536cee072ef964a89772218f1e23a81c29c7480
threi085 <128632370+threi085@users.noreply.github.com>
2023-06-12 15:03:10 -0300

    Update funcoeskotlin.kt

---
+++ funcoeskotlin.kt
@@ -0,0 +1,9 @@
+//cria uma função(metodo)que escreve na tela
+fun minhaFuncao(){
+  println("minha função escreve na tela")
+}
+
+fun main(args: Array<String>) {
+  minhaFuncao()
+  minhaFuncao()
+}
