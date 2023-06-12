c8aa98bde8718388717dfa97430a22f627483f3b
threi085 <128632370+threi085@users.noreply.github.com>
2023-06-12 16:24:00 -0300

    Create THIAGO.JAVA

---
+++ THIAGO.JAVA
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo!");
+    int myNum = 6;
+    float myFloatNum = 4.99f;
+    char myLetter = 'P';
+    boolean myBool = false;
+    String myText = "Só salvinho";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText); 
+    // parte dois do código
+    System.out.println(myText + myBool);
+    int x = 8;
+    float y = x;
+    System.out.println(x);
+    System.out.println(y);
+    double myDouble = 5.99d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
